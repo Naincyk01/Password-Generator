3aa6f744d0344e1eddffa07a8f68f9b722f143a3
Naincy <naincyk15042004@gmail.com>
2024-04-09 23:08:48 +0530

    Formatted the code

--- PASSWORD.js
+++ PASSWORD.js
@@ -1,9 +1,9 @@
-let upperset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-let lowerset = "abcdefghijklmnopqrstuvwxyz"
-let numberset = "1234567890"
-let symbolset = "!@#$%^&*_"
+let upperset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerset = "abcdefghijklmnopqrstuvwxyz";
+let numberset = "1234567890";
+let symbolset = "!@#$%^&*_";
 
-let passBox = document.getElementById("output")
+let passBox = document.getElementById("output");
 let generatebtn = document.getElementById("generate");
 let lowerCaseInput = document.getElementById("lowercase");
 let upperCaseInput = document.getElementById("uppercase");
@@ -14,65 +14,54 @@ let copyInput = document.getElementById("icons");
 let refresh = document.getElementById("strength");
 
 const updateSlider = () => {
-    document.getElementById("numb").innerText=totalchar.value;
-}
+  document.getElementById("numb").innerText = totalchar.value;
+};
 updateSlider();
-totalchar.addEventListener("input",updateSlider)
+totalchar.addEventListener("input", updateSlider);
 
-function CopyToClipboard(value){
-    navigator.clipboard.writeText(value);
+function CopyToClipboard(value) {
+  navigator.clipboard.writeText(value);
 }
 
-function RefreshPage(){
-    window.location.reload()
+function RefreshPage() {
+  window.location.reload();
 }
-copyInput.addEventListener("click",function(){
-    CopyToClipboard(passBox.innerText)
-})
-refresh.addEventListener("click",function(){
-    RefreshPage()
-})
+copyInput.addEventListener("click", function () {
+  CopyToClipboard(passBox.innerText);
+});
+refresh.addEventListener("click", function () {
+  RefreshPage();
+});
 const getRandomData = (dataSet) => {
-    return dataSet[Math.floor(Math.random()*dataSet.length)]
-}
+  return dataSet[Math.floor(Math.random() * dataSet.length)];
+};
 const generatePassword = (password = "") => {
-if(upperCaseInput.checked){
-    password += getRandomData(upperset)
-}
-if(lowerCaseInput.checked){
-    password += getRandomData(lowerset)
-}
-if(numberInput.checked){
-    password += getRandomData(numberset)
-}
-if(specialCharsInput.checked){
-    password += getRandomData(symbolset)
-}
-if(password.length< totalchar.value){
-    return generatePassword(password)
-}
-   passBox.innerText = cutString(password, totalchar.value);
-}
-generatebtn.addEventListener("click",function(){
-    generatePassword();
-})
+  if (upperCaseInput.checked) {
+    password += getRandomData(upperset);
+  }
+  if (lowerCaseInput.checked) {
+    password += getRandomData(lowerset);
+  }
+  if (numberInput.checked) {
+    password += getRandomData(numberset);
+  }
+  if (specialCharsInput.checked) {
+    password += getRandomData(symbolset);
+  }
+  if (password.length < totalchar.value) {
+    return generatePassword(password);
+  }
+  passBox.innerText = cutString(password, totalchar.value);
+};
+generatebtn.addEventListener("click", function () {
+  generatePassword();
+});
 
-
-function cutString(str, num){
-if(str.length>num){
-    let subStr = str.substring(0,num);
+function cutString(str, num) {
+  if (str.length > num) {
+    let subStr = str.substring(0, num);
     return subStr;
-}
-else{
+  } else {
     return str;
+  }
 }
-}
-
-
-
-
-
-
-
-
-
